e90eeda56f0bede7199acce84d8659aada9e5515
perplexingcode <perplexingcode@gmail.com>
2022-10-05 19:00:38 +0700

    base template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  <div class="center">
+    <img class="logo" alt="Divinity logo" src="./assets/logo.png">
+  </div>
+  
+  <ExampleItem tt="An example item." dt="Lorem ipsum dolor sit amet."/>
+  </template>
+  
+  <script>
+  import ExampleItem from './components/ExampleItem.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      ExampleItem
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  }
+  </script>
+  
+  <style>
+  #app {
+    font-family: comfortaa, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #051c2b ;
+    margin-top: 60px;
+  }
+  
+  .logo {
+  width: 10rem;
+  height: 10rem;
+  }
+  
+  .center {
+    text-align: center;
+  }
+  
+  .tt {
+    background-color:  #051c2b ;
+    color: #fff ;
+    padding: 2px 0 2px 0
+  }
+  </style>
+  
